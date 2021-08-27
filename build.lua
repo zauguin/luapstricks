e38c470e89118b4fdb191b8ed7d4eb6c63f88f27
Marcel Fabian Krüger <tex@2krueger.de>
2021-08-27 05:49:09 +0200

    Add testsfiles and build.lua

---
+++ build.lua
@@ -0,0 +1,25 @@
+module = "luapstricks"
+
+typesetexe = "lualatex"
+stdengine = "luatex"
+checkengines = {"luatex"}
+
+installfiles = {"luapstricks.lua"}
+sourcefiles = {"luapstricks.lua"}
+-- typesetfiles = {"luapstricks.tex"}
+
+tdsroot = "luatex"
+
+uploadconfig = {
+  pkg = module,
+  version = "v0.1",
+  author = "Marcel Krüger",
+  license = "lppl1.3",
+  summary = "A pstricks backend for LuaLaTeX",
+  ctanPath = "/macros/luatex/generic/luapstricks",
+  update = false,
+  repository = "https://github.com/zauguin/luapstricks",
+  bugtracker = "https://github.com/zauguin/luapstricks/issues",
+  topic = {"luatex", "pstricks"},
+  announcement_file = "announce",
+}
