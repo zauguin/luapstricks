01b10be03c7d2f81471c2e9dbf3c0d4c63164b92
Marcel Fabian Krüger <tex@2krueger.de>
2021-09-17 23:39:37 +0200

    Update build.lua

--- build.lua
+++ build.lua
@@ -17,10 +17,11 @@ uploadconfig = {
   author = "Marcel Krüger",
   license = "lppl1.3",
   summary = "A pstricks backend for LuaLaTeX",
-  ctanPath = "/macros/luatex/generic/luapstricks",
+  ctanPath = "/graphics/pstricks/contrib/luapstricks",
   update = false,
   repository = "https://github.com/zauguin/luapstricks",
   bugtracker = "https://github.com/zauguin/luapstricks/issues",
   topic = {"luatex", "pstricks"},
   announcement_file = "announce",
+  description = "luapstricks enables the use of PSTricks directly in LuaLaTeX documents, without invoking external programmes. Therefore it does not require shell escape to be enabled or special environments and instead allows PSTricks to be used exactly like in dvips based documents.",
 }
